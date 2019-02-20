c1ae89895934543d9008701adb0996242b36fc51
unknown <felix.robaglia@gmail.com>
2019-02-20 22:27:08 +0100

    add typewriter effect in explanation section

--- js/main.js
+++ js/main.js
@@ -1,18 +1,52 @@
-let sections = document.querySelectorAll('section');
-let svgs = document.querySelectorAll('.pattern');
+let sections = document.querySelectorAll("section");
+let svgs = document.querySelectorAll(".pattern");
+
+let container1 = document.querySelector(".first-typed");
+let container2 = document.querySelector(".second-typed");
+let container3 = document.querySelector(".third-typed");
+
+var i = 0;
+let j = 0;
+let k = 0;
+var txt1 =
+  "Thermochromic ink with an ilimited color palette for your customizable patterns";
+var txt2 =
+  "50 % polyurethane, 50 % polyester, a strong fabric totally waterproof with sealed stitching and liner";
+var txt3 =
+  "Four available windproof models, with removable hood and two big pockets";
+
+function typeWriterOne() {
+  if (i < txt1.length) {
+    container1.innerHTML += txt1.charAt(i);
+    i++;
+    setTimeout(typeWriterOne, 30);
+  }
+}
+function typeWriterTwo() {
+  if (j < txt2.length) {
+    container2.innerHTML += txt2.charAt(j);
+    j++;
+    setTimeout(typeWriterTwo, 25);
+  }
+}
+function typeWriterThree() {
+  if (k < txt1.length) {
+    container3.innerHTML += txt3.charAt(k);
+    k++;
+    setTimeout(typeWriterThree, 35);
+  }
+}
 
 setTimeout(() => {
-  document.querySelector('.loader').style.opacity = 1;
-  
+  document.querySelector(".loader").style.opacity = 1;
 }, 100);
 
 setTimeout(() => {
-  document.querySelector('.loader').style.opacity = 0;
-  
-}, 2100);
+  document.querySelector(".loader").style.opacity = 0;
+}, 2200);
 setTimeout(() => {
-  document.querySelector('header').style.opacity = 1;
-}, 4100);
+  document.querySelector("header").style.opacity = 1;
+}, 4200);
 
 sections.forEach(function(section) {
   var element = section;
@@ -20,20 +54,21 @@ sections.forEach(function(section) {
   var waypoint = new Waypoint({
     element: element,
     handler: function(direction) {
-      element.classList.add('is-reached');
+      element.classList.add("is-reached");
     },
-    offset: '75%'
+    offset: "75%"
   });
 });
 
-svgs.forEach(function(svg) {
-  var pattern = svg;
-
-  var waypoint = new Waypoint({
-    element: pattern,
-    handler: function(direction) {
-      pattern.classList.add('is-reached');
-    },
-    offset: '75%'
-  });
+new Waypoint({
+  element: container1,
+  handler: function(direction) {
+    svgs.forEach(function(svg) {
+      svg.classList.add("is-reached");
+    });
+    typeWriterOne();
+    typeWriterTwo();
+    typeWriterThree();
+  },
+  offset: '50%'
 });

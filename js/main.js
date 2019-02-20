b19a49439fe2c655b78ec2e695144095edfe8368
unknown <felix.robaglia@gmail.com>
2019-02-20 18:07:49 +0100

    nav's rectangle moving on hover

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+let sections = document.querySelectorAll('section');
+
 setTimeout(() => {
   document.querySelector('body').style.opacity = 1;
 }, 500);
+
+sections.forEach(function(section) {
+  var element = section;
+
+  var waypoint = new Waypoint({
+    element: element,
+    handler: function(direction) {
+      element.classList.add('is-reached');
+    },
+    offset: '75%'
+  });
+});
